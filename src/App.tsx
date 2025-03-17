e48366bf478e3f079e33ae21d1fcbbf3f4701151
Fabrice Boyer <fabrice.boyer@gmail.com>
2025-03-17 22:41:16 +0100

    ernomy improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import wordsData from "./data/database_lemma.generated.json";
 
@@ -8,11 +8,13 @@ interface Word {
   leaf_char: string;
   backlinks: string[];
 }
+
 const CharSequenceSearch = () => {
   const [query, setQuery] = useState("");
   const [sequences, setSequences] = useState<Word[]>([]);
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [highlightedIndex, setHighlightedIndex] = useState<number>(-1);
+  const prevQuery = usePrevious(query);
 
   useEffect(() => {
     const loadWords = () => {
@@ -24,27 +26,66 @@ const CharSequenceSearch = () => {
   }, []); // Empty dependency array ensures this runs once on mount
 
   useEffect(() => {
-    setHighlightedIndex(-1);
     var word = sequences.find((w) => w.id === query);
     if (word) {
-      var backlinks = word.backlinks;
-      if (backlinks.length == 1) {
-        // complete more than one char
-        setQuery(backlinks[0]);
-        return;
-      }
-      setSuggestions(backlinks);
+      var backlinks: string[] = word.backlinks;
+      var expandedBacklinks = expandBacklinks(backlinks);
+      setSuggestions(expandedBacklinks);
+      setHighlightedIndex(0);
     } else {
-      setQuery(query.substring(0, query.length - 1)); // wrong words refused
-      setSuggestions([]);
+      if (query.length > 0) {
+        setQuery(prevQuery); // wrong words refused
+      } else {
+        // reset
+        setSuggestions([]);
+        setHighlightedIndex(-1);
+      }
     }
   }, [query, sequences]);
 
+  function expandBacklinks(backlinks: string[]): string[] {
+    var expandedBacklinks: string[] = [];
+    backlinks.forEach((b) => {
+      var word: Word | undefined = sequences.find((w) => w.id === b);
+      while (word != undefined && word.backlinks.length == 1) {
+        word = sequences.find((w) => w.id === word.backlinks[0]);
+      }
+      if (!expandedBacklinks.includes(word.id)) {
+        expandedBacklinks.push(word.id);
+      }
+    });
+    return expandedBacklinks;
+  }
+
+  function usePrevious(value: string) {
+    const ref = useRef("");
+    useEffect(() => {
+      ref.current = value;
+    }, [value]);
+    return ref.current;
+  }
+
+  function clamp(num: number, lower: number, upper: number) {
+    return Math.min(Math.max(num, lower), upper);
+  }
+
+  function clampHighlightedIndex(value: number) {
+    const max = suggestions.length - 1;
+    const min = 0;
+    if (value > max) {
+      return min;
+    } else if (value < min) {
+      return max;
+    }
+    return value;
+    //return clamp(value, 0, suggestions.length - 1);
+  }
+
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "ArrowDown") {
-      setHighlightedIndex((prev) => Math.min(prev + 1, suggestions.length - 1));
+      setHighlightedIndex((prev) => clampHighlightedIndex(prev + 1));
     } else if (e.key === "ArrowUp") {
-      setHighlightedIndex((prev) => Math.max(prev - 1, 0));
+      setHighlightedIndex((prev) => clampHighlightedIndex(prev - 1));
     } else if (e.key === "Enter" && highlightedIndex >= 0) {
       setQuery(suggestions[highlightedIndex]);
       setSuggestions([]);
@@ -61,10 +102,8 @@ const CharSequenceSearch = () => {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyDown={handleKeyDown}
+        autoFocus
       />
-      {/* <button className="" onClick={() => setQuery("")}>
-        Clear
-      </button> */}
       <div className="word-list">
         {suggestions.length === 0 ? (
           <div style={{ textAlign: "center" }}>
